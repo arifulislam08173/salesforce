3d57d737be6a1ade6aea53d438f09ccc13d87d21
arifulislam08 <5seariful15@gmail.com>
2025-05-17 11:28:56 +0600

    update config.js

--- backend/Server/config.js
+++ backend/Server/config.js
@@ -1,12 +1,10 @@
-// server/config.js
-
 const { Sequelize } = require('sequelize');
 
 // Create Sequelize instance
 const sequelize = new Sequelize('salesforce', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
-  logging: false // Set to true for debugging
+  logging: false
 });
 
 module.exports = sequelize;
